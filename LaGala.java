2df0a7100b46300616e8a997c15763ece7a53171
Francesco La Gala <francesco.lagala8190@gmail.com>
2018-06-18 23:46:21 +0200

    homework

---
+++ LaGala.java
@@ -0,0 +1,15 @@
+
+public class LaGala {
+
+	public static void main(String[] args) {
+		
+		final String NOME = "Francesco";
+		final String COGNOME = "La Gala";
+		
+		System.out.println("Il mio nome è: "+NOME+" "+COGNOME);
+
+
+
+	}
+
+}
