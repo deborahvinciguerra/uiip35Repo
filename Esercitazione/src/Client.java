571ea18295025fd1840ccd29180ec48b775b0cbb
Antonio Pagliaro <antonio.pagliaro1988@gmail.com>
2018-06-19 16:41:20 +0200

    modifica client Pagliaro

--- Esercitazione/src/Client.java
+++ Esercitazione/src/Client.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Client {
@@ -16,6 +17,19 @@ public class Client {
         
         System.out.println("Nome= "+p1.getNome()+"\nCognome= "+p1.getCognome());
         System.out.println(p1.getIndirizzo().toString()+"Età= "+u1.calcolaEta(p1));
+        
+        
+        
+        Indirizzo i2= new Indirizzo ("vico lopa",18,"ostia","80032","RO","lazio");
+
+        Persona p2= new Persona ("antonio","Pagliaro",d1,i2);
+        
+        ArrayList<Persona> listaPersone=new ArrayList<Persona>();
+        listaPersone.add(p1);
+        listaPersone.add(p2);
+        
+        
+        System.out.println(ResidentiCampania.listaPersoneCampania(listaPersone));
         
         
        
