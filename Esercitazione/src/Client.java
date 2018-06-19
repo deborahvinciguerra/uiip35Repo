9b66917c8e93f7f39d26816b09e43b0b64eed065
Giacomo Ferraiuolo <giacomo.ferraiuolo1989@gmail.com>
2018-06-19 16:10:50 +0200

    Stampa nome e cognome

--- Esercitazione/src/Client.java
+++ Esercitazione/src/Client.java
@@ -1,18 +1,37 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Client {
 
 	public static void main(String[] args) {
 
 		//Indirizzo(String via, int civico, String citta, String cap, String provincia, String regione)
-        Indirizzo i1= new Indirizzo ("via roma",22,"napoli","80125","NA","campania");
-        
-        Date d1= new Date(1990,4,22);
-        
-        
-        //Persona(String nome, String cognome, Date dataDiNascita, Indirizzo indirizzo, String regione)
-        Persona p1= new Persona ("francesco","la gala",d1,i1);
+		Indirizzo i1= new Indirizzo ("via roma",22,"napoli","80125","NA","campania");
 
+		Date d1= new Date(1990,4,22);
+
+
+		//Persona(String nome, String cognome, Date dataDiNascita, Indirizzo indirizzo, String regione)
+		Persona p1= new Persona ("francesco","la gala",d1,i1);
+
+		// Creazione persona - Giacomo Ferraiuolo
+		Indirizzo indirizzo = new Indirizzo("Via Carmelo Pezzullo",99,"Frattamaggiore","80027","Napoli","Campania");
+		Date nascita = new Date("09/05/1989");
+		Persona giacomo = new Persona("Giacomo","Ferraiuolo",nascita,indirizzo);
+		Utility u = new Utility();
+		int eta = u.calcolaEta(nascita);
+
+		System.out.println(giacomo.getNome()+" "+giacomo.getCognome()+"\n"+
+				indirizzo.toString()+"Età = "+eta+" anni");
+
+		ArrayList<Persona> listaPersone = new ArrayList<Persona>();
+		listaPersone.add(giacomo);
+
+		for(int i=0; i<listaPersone.size(); i++)
+			System.out.println(ResidentiCampania.listaPersoneCampania(listaPersone).get(i).getNome()+" "+
+					ResidentiCampania.listaPersoneCampania(listaPersone).get(i).getCognome()+"\n"+
+					ResidentiCampania.listaPersoneCampania(listaPersone).get(i).getIndirizzo().toString());
 	}
 
 }
