21f72f2ac2684e7546c957081bf3d8b8e078d8af
Fabio Spagnuolo <fabiospagnuolo1995@gmail.com>
2018-06-19 15:23:35 +0200

    modifica errore sintassi

--- Esercitazione/src/ResidentiCampania.java
+++ Esercitazione/src/ResidentiCampania.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
-public class ResidentiCampania {
+public  class ResidentiCampania {
 
 	public  static ArrayList<Persona> listaPersoneCampania(ArrayList<Persona> listaPersone){
 
 		ArrayList<Persona> tmp=new ArrayList<Persona>();
 
 		for(Persona p: listaPersone)
-			if (p.getIndirizzo().getRegione().equalsIgnorecase("campania"))
+			if (p.getIndirizzo().getRegione().equalsIgnoreCase("campania"))
 				tmp.add(p);
 
 		return tmp; 
