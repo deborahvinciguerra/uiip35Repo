b672dbd86ca50cd6c403651b2e307c9a9d4e1d9e
domenico.guida <domenico.guida1806@gmail.com>
2018-06-19 17:32:04 +0200

    File modificatto 17:30

--- Esercitazione/src/Client.java
+++ Esercitazione/src/Client.java
@@ -10,83 +10,42 @@ import java.util.*;
 public class Client {
 
 	public static void main(String[] args) {
-		
-	 
+		 
+		//Creazione Francesco La Gala
 		//Indirizzo(String via, int civico, String citta, String cap, String provincia, String regione)
         Indirizzo i1= new Indirizzo ("vico lopa",18,"casamarciano","80032","NA","campania");
-        
         Date d1= new Date("12/12/1990");
-        
         //Persona(String nome, String cognome, Date dataDiNascita, Indirizzo indirizzo, String regione)
         Persona p1= new Persona ("francesco","la gala",d1,i1);
-        
-
-        Date x = new Date("18/06/1989");
-        
-        Indirizzo y = new Indirizzo("Via Campitiello", 4, "Limatola", "82030", "BN", "Campania");
-        
-
-        Persona d = new Persona ("Domenico", "Guida", x, y);
-        
-        System.out.println("Nome e Cognome: "+ d.getNome() + " " + d.getCognome());
-        System.out.println("Indirizzo: " + d.getIndirizzo().toString());
-        System.out.println("Età: "+ Utility.calcolaEta(d));
-        
-  
-      
+        System.out.println(p1.getIndirizzo().toString()+"Età= "+ Utility.calcolaEta(p1));
         System.out.println("Nome= "+p1.getNome()+"\nCognome= "+p1.getCognome());
-        System.out.println(p1.getIndirizzo().toString()+"Età= "+Utility.calcolaEta(p1));
         
-       
-        List<Persona> lista = new ArrayList<Persona>();
-
-
+        // Creazione Domenico Guida
+        Date dataDomenico = new Date("18/06/1989");
+        Indirizzo indirizzoDomenico = new Indirizzo("Via Campitiello", 4, "Limatola", "82030", "BN", "Campania");
+        Persona personaDomenico = new Persona ("Domenico", "Guida", dataDomenico, indirizzoDomenico);
+        System.out.println("Nome e Cognome: "+ personaDomenico.getNome() + " " + personaDomenico.getCognome());
+        System.out.println("Indirizzo: " + personaDomenico.getIndirizzo().toString());
+        System.out.println("Età: "+ Utility.calcolaEta(personaDomenico));
 
 		// Creazione persona - Giacomo Ferraiuolo
 		Indirizzo indirizzo = new Indirizzo("Via Carmelo Pezzullo",99,"Frattamaggiore","80027","Napoli","Campania");
 		Date nascita = new Date("09/05/1989");
 		Persona giacomo = new Persona("Giacomo","Ferraiuolo",nascita,indirizzo);
 		int eta = Utility.calcolaEta(nascita);
-
 		System.out.println(giacomo.getNome()+" "+giacomo.getCognome()+"\n"+
 				indirizzo.toString()+"Età = "+eta+" anni");
 
-		ArrayList<Persona> listaPersone = new ArrayList<Persona>();
-		listaPersone.add(giacomo);
-
-
-
+		//Creazione Gabriella Conte
         Persona gabriella = new Persona("Gabriella","Conte",new Date(1988,5,18),new Indirizzo("via amatucci",14,"Avellino","83100","AV","Campania"));
- 
         System.out.println(gabriella.getNome()+" "+gabriella.getCognome());
         System.out.println("Età: "+Utility.calcolaEta(new Date("18/05/1988"))+" anni");
         System.out.println("Indirizzo: "+gabriella.getIndirizzo());
-
-        lista.add(gabriella);
-        lista.add(d);
-        lista.add(p1);
-        for(int i=0;i<lista.size();i++)
-        	System.out.println(ResidentiCampania.listaPersoneCampania((ArrayList<Persona>) lista).get(i).getNome());
-
-
-
-        listaPersone.add(gabriella);
         
-        
+        //Creazione Antonio Pagliaro
         Indirizzo i2= new Indirizzo ("vico lopa",18,"ostia","80032","RO","lazio");
-
         Persona p2= new Persona ("antonio","Pagliaro",d1,i2);
-        
         ArrayList<Persona> persone=new ArrayList<Persona>();
-        persone.add(p1);
-        persone.add(p2);
-        
-        
-        ArrayList<Persona> personeCampane=ResidentiCampania.listaPersoneCampania(persone);
-        System.out.println("Persone residenti in campania");
-        for(Persona p : personeCampane) {
-        	System.out.println("Nome: "+p.getNome()+" Cognome: "+p.getCognome());
-        }
         
         //creazione fabio spagnuolo
         Date d3=new Date("27/06/1995");
@@ -94,39 +53,36 @@ public class Client {
         Persona p3=new Persona("Fabio", "spagnuolo", d3 , i3);
          persone.add(p3);
         
-        
+        // Creazione persona - Fabio Irno
+        Indirizzo indirizzo1 = new Indirizzo("Corso Caracciolo",70,"Mercato San Severino","84085","Salerno","Campania");
+ 		Date nascita1 = new Date("01/03/1987");
+ 		Persona fabio = new Persona("Fabio","Irno",nascita1,indirizzo1);
+ 			
         
        
-     // Creazione persona - Fabio Irno
-        Indirizzo indirizzo1 = new Indirizzo("Corso Caracciolo",70,"Mercato San Severino","84085","Salerno","Campania");
-		Date nascita1 = new Date("01/03/1987");
-		Persona fabio = new Persona("Fabio","Irno",nascita1,indirizzo1);
-			
+     
 		System.out.println(fabio.getNome()+" "+fabio.getCognome()+"\n"+
 				indirizzo1.toString()+"Età = "+Utility.calcolaEta(nascita1)+" anni");
 		
 				
-		listaPersone.add(fabio);
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("\n");
-        
-		// Stampa della lista delle persone residenti in campania
-		List<Persona> personeTrovate = new ArrayList<Persona>();
-		personeTrovate = ResidentiCampania.listaPersoneCampania(listaPersone);
-		
-		for(int i=0; i<listaPersone.size(); i++)
-			System.out.println(personeTrovate.get(i).getNome()+" "+
-					personeTrovate.get(i).getCognome()+"\n"+
-					personeTrovate.get(i).getIndirizzo().toString());
 
-
+		ArrayList<Persona> lista = new ArrayList<Persona>();
 		
+		lista.add(gabriella);
+        lista.add(personaDomenico);
+        lista.add(p1);
+        lista.add(giacomo);
+        lista.add(fabio);
+        lista.add(p2);
+        lista.add(p3);
+        lista.add(fabio);
+       
+        ArrayList<Persona> residentiInCampania=ResidentiCampania.listaPersoneCampania(lista);
+        System.out.println("Persone residenti in campania");
+        for(Persona p : residentiInCampania) {
+        	System.out.println("Nome: "+p.getNome()+" Cognome: "+p.getCognome());
+        }
+    
 
 	}
 
