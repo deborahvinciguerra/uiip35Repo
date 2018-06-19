da827bb52538639b53c8b6d1fee840fd51ec35ee
Francesco La Gala <francesco.lagala8190@gmail.com>
2018-06-19 13:24:54 +0200

    integrazione indirizzo

--- Esercitazione/src/Persona.java
+++ Esercitazione/src/Persona.java
@@ -4,70 +4,62 @@ public class Persona {
 	private String nome;
 	private String cognome;
 	private String dataDiNascita;
-	//indirizzo es via roma 21, 80032, nola (NA)
-	private String indirizzo;
+	private Indirizzo indirizzo;
 	private String regione;
 	
-	
-	public Persona(String nome, String cognome, String dataDiNascita, String indirizzo, String regione) {
+	public Persona(String nome, String cognome, String dataDiNascita, Indirizzo indirizzo, String regione) {
 		this.nome = nome;
 		this.cognome = cognome;
-		this.dataDiNascita= dataDiNascita;
+		this.dataDiNascita = dataDiNascita;
 		this.indirizzo = indirizzo;
 		this.regione = regione;
 	}
-
 
 	public String getNome() {
 		return nome;
 	}
 
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 
 	public String getCognome() {
 		return cognome;
 	}
 
-
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
-
-
-
-
 
 	public String getDataDiNascita() {
 		return dataDiNascita;
 	}
 
-
 	public void setDataDiNascita(String dataDiNascita) {
 		this.dataDiNascita = dataDiNascita;
 	}
 
-
-	public String getIndirizzo() {
+	public Indirizzo getIndirizzo() {
 		return indirizzo;
 	}
 
-
-	public void setIndirizzo(String indirizzo) {
+	public void setIndirizzo(Indirizzo indirizzo) {
 		this.indirizzo = indirizzo;
 	}
-
 
 	public String getRegione() {
 		return regione;
 	}
 
-
 	public void setRegione(String regione) {
 		this.regione = regione;
 	}
+	
+	
+	
+	
+	
+	
+	
 
 }
