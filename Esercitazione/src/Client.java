b1a4c497ceaef8f2d0240a3a014aba054637e200
Gabriella Conte <gabriella.conte47@gmail.com>
2018-06-19 16:10:51 +0200

    modifiche apportate

--- Esercitazione/src/Client.java
+++ Esercitazione/src/Client.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Client {
@@ -12,6 +15,17 @@ public class Client {
         
         //Persona(String nome, String cognome, Date dataDiNascita, Indirizzo indirizzo, String regione)
         Persona p1= new Persona ("francesco","la gala",d1,i1);
+
+        List<Persona> lista = new ArrayList<Persona>();
+        Persona gabriella = new Persona("Gabriella","Conte",new Date(1988,5,18),new Indirizzo("via amatucci",14,"Avellino","83100","AV","Campania"));
+        Utility ut = new Utility();
+        System.out.println(gabriella.getNome()+" "+gabriella.getCognome());
+        System.out.println("Età: "+ut.calcolaEta(new Date("18/05/1988"))+" anni");
+        System.out.println("Indirizzo: "+gabriella.getIndirizzo());
+        lista.add(gabriella);
+        
+        for(int i=0;i<lista.size();i++)
+        	System.out.println(ResidentiCampania.listaPersoneCampania((ArrayList<Persona>) lista).get(i).getNome());
 
 	}
 
