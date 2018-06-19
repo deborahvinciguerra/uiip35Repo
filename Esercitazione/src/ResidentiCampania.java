47d6fa2de1262203b4065f5eb7529d0b0e19f76e
Fabio Spagnuolo <fabiospagnuolo1995@gmail.com>
2018-06-19 14:51:30 +0200

    metodo statico

--- Esercitazione/src/ResidentiCampania.java
+++ Esercitazione/src/ResidentiCampania.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 
 public class ResidentiCampania {
 
-	public ArrayList<Persona> listaPersoneCampania(ArrayList<Persona> listaPersone){
-		
+	public  static ArrayList<Persona> listaPersoneCampania(ArrayList<Persona> listaPersone){
+
 		ArrayList<Persona> tmp=new ArrayList<Persona>();
-		
+
 		for(Persona p: listaPersone)
 			if (p.getIndirizzo().getRegione().equalsIgnorecase("campania"))
 				tmp.add(p);
-		
-		return tmp;
-		
-		
+
+		return tmp; 
+
+
 	}
-	
-	
-	
+
+
+
 }
