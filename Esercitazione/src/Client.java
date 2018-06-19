e1e89ea7b115cbb929253c1b5a3688de168b0c40
domenico.guida <domenico.guida1806@gmail.com>
2018-06-19 16:13:25 +0200

    Modifica Domenico

--- Esercitazione/src/Client.java
+++ Esercitazione/src/Client.java
@@ -1,4 +1,6 @@
+import java.io.LineNumberInputStream;
 import java.util.Date;
+import java.util.*;
 
 public class Client {
 
@@ -12,6 +14,30 @@ public class Client {
         
         //Persona(String nome, String cognome, Date dataDiNascita, Indirizzo indirizzo, String regione)
         Persona p1= new Persona ("francesco","la gala",d1,i1);
+        
+        Date x = new Date("18/06/1989");
+        
+        Indirizzo y = new Indirizzo("Via Campitiello", 4, "Limatola", "82030", "BN", "Campania");
+        
+        Persona d = new Persona ("Domenico", "Guida", x, y);
+        
+        Utility u = new Utility();
+        
+        System.out.println("Nome e Cognome: "+ d.getNome() + " " + d.getCognome());
+        System.out.println("Indirizzo: " + d.getIndirizzo().toString());
+        System.out.println("Età: "+ u.calcolaEta(d));
+        
+        ArrayList <Persona> lista = new ArrayList<Persona>();
+        lista.add(d);
+        ResidentiCampania.listaPersoneCampania(lista);
+        System.out.println("Persone residenti in Campania: ");
+        for(Persona p : lista) {
+        System.out.println(p.getNome() + " " + p.getCognome()+"\n");
+        }
+       
+        
+        
+        
 
 	}
 
