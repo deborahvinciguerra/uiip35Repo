42925d8f5e4dda47515c1cae58b0e048f805dc19
domenico.guida <domenico.guida1806@gmail.com>
2018-06-19 13:37:10 +0200

    Creazione classe Utility calcolo età

--- Esercitazione/src/Persona.java
+++ Esercitazione/src/Persona.java
@@ -1,17 +1,6 @@
-import java.util.Calendar;
-import java.util.Scanner;
+
 
 public class Persona {
 	
-	public int calcolaEta() {
-	Scanner input = new Scanner(System.in);
-	System.out.println("Inserisci l'anno di nascita: ");
-	int nascita = input.nextInt();
-	int eta;
-	Calendar date = Calendar.getInstance();
-	int anno = date.get(Calendar.YEAR);
-	eta = anno - nascita;
-	return eta;
-	}
 
 }
