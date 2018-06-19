d454a37631fb16488152514a0fb1dd9b851187ee
domenico.guida <domenico.guida1806@gmail.com>
2018-06-19 13:12:22 +0200

    metodo per il calcolo dell'età

--- Esercitazione/src/Persona.java
+++ Esercitazione/src/Persona.java
@@ -1,4 +1,17 @@
+import java.util.Calendar;
+import java.util.Scanner;
 
 public class Persona {
+	
+	public int calcolaEta() {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Inserisci l'anno di nascita: ");
+	int nascita = input.nextInt();
+	int eta;
+	Calendar date = Calendar.getInstance();
+	int anno = date.get(Calendar.YEAR);
+	eta = anno - nascita;
+	return eta;
+	}
 
 }
