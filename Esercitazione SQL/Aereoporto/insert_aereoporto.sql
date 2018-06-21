c152c7d600ae95c7d6eb9550433b1168d50f2691
Angelo Clemente <angeloclemente12@libero.it>
2018-06-21 11:30:04 +0200

    inserimento Angelo

---
+++ Esercitazione SQL/Aereoporto/insert_aereoporto.sql
@@ -0,0 +1,17 @@
+# Angelo Clemente
+
+insert into aeroporto (citta, nazione, npiste) values
+                       ('Parigi', 'Francia', 6),
+                       ('Napoli', 'Italia' , 4);
+                       
+                       
+                       
+insert into aereo (tipoAereo, nPass, quantitaMerci) values
+                   ('DC-3', 50, 20);
+                   
+                   
+insert into volo (idVolo, giornoSett, cittaPart, cittaArr, tipoAereo, oraPartenza, oraArrivo) values
+				(1, 'lunedi' , 'Parigi' , 'Napoli', 'DC-3', 06, 06);
+                
+                
+# Angelo Clemente
