e5338103d330ca0d7dface3b31055ec639af5ae5
Francesco La Gala <francesco.lagala8190@gmail.com>
2018-06-21 11:24:44 +0200

    inserimento la gala

---
+++ Esercitazione SQL/Aereoporto/insert_aereoporto.sql
@@ -0,0 +1,11 @@
+INSERT INTO aeroporto (citta, nazione, nPiste) VALUES
+					('bologna', 'italia',4),
+                    ('bergamo', 'italia',3);
+					
+					
+                    
+INSERT INTO aereo (tipoAereo, nPass, quantitaMerci) VALUES
+				('787', 50, 2341);
+				
+INSERT INTO volo (idVolo, giornoSett, cittaPart, cittaArr, tipoAereo, oraPartenza, oraArrivo) VALUES
+				(143, 'venerdi', 'bologna', 'bergamo', '787', 930, 1130);
