ea6549e9c823204c68f72efb70bb868597cbe52a
Fabio Irno <fabioirno87@gmail.com>
2018-06-21 11:26:06 +0200

    inserimento volo fabio

---
+++ Esercitazione SQL/Aereoporto/insert_aereoporto.sql
@@ -0,0 +1,15 @@
+#Inserimento di un volo da torino, Fabio Irno
+
+INSERT INTO aeroporto(citta,nazione,npiste) VALUES
+						('Torino','Italia',10),
+                        ('Palermo','Italia',2);
+                        
+INSERT INTO aereo(tipoAereo,nPass,quantitaMerci) VALUES
+						('747',250,300);
+                        
+INSERT INTO volo(idVolo,giornoSett,cittaPart,cittaArr,tipoAereo,oraPartenza,oraArrivo) VALUES
+						(10,'Mercoledì','Torino','Palermo','747',10,14);
+                        
+                        
+	
+                        
