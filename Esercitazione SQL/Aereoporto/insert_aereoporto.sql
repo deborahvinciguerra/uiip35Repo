8d64b735b60650f79795e1ffec0b3ebd95907473
Giacomo Ferraiuolo <giacomo.ferraiuolo1989@gmail.com>
2018-06-21 11:30:25 +0200

    aggiunta volo Ferraiuolo

--- Esercitazione SQL/Aereoporto/insert_aereoporto.sql
+++ Esercitazione SQL/Aereoporto/insert_aereoporto.sql
@@ -9,3 +9,19 @@ INSERT INTO aereo (tipoAereo, nPass, quantitaMerci) VALUES
 				
 INSERT INTO volo (idVolo, giornoSett, cittaPart, cittaArr, tipoAereo, oraPartenza, oraArrivo) VALUES
 				(143, 'venerdi', 'bologna', 'bergamo', '787', 930, 1130);
+
+INSERT INTO aeroporto (citta, nazione, nPiste) VALUES
+					('bologna', 'italia',4),
+                    ('bergamo', 'italia',3);
+					
+					
+# Inserimento Giacomo Ferraiuolo
+INSERT INTO aeroporto (citta, nazione, nPiste) VALUES
+					('Napoli', 'Italia',2),
+                    ('Milano', 'Italia',4);
+                    
+INSERT INTO aereo (tipoAereo, nPass, quantitaMerci) VALUES
+					('A320', 200, 4000);
+                    
+INSERT INTO volo (idVolo, giornoSett, cittaPart, cittaArr, tipoAereo, oraPartenza, oraArrivo) VALUES
+				(1, 'Lunedì', 'Napoli', 'Milano', 'A320', 9, 11);
