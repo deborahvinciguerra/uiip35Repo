674ca90daced25f1078c47d8136f31ddf8e1e1b4
domenico.guida <domenico.guida1806@gmail.com>
2018-06-21 11:29:20 +0200

    Volo Domenico

---
+++ Esercitazione SQL/Aereoporto/insert_aereoporto.sql
@@ -0,0 +1,14 @@
+# #####################
+# DUMPING DATA VOLO 
+# #####################
+#Domenico Guida
+
+INSERT INTO aeroporto (citta, nazione, nPiste) VALUES
+					  ('Rio de Janeiro', 'Brasile',  4),
+                      ('Tokyo', 'Brasile',  4);
+                       
+INSERT INTO aereo (tipoAereo, nPass, quantitaMerci) Values
+                  ( 'B737', 100, 2000);
+   
+INSERT INTO volo (idVolo, giornoSett, cittaPart, cittaArr, tipoAereo, oraPartenza, oraArrivo) Values
+				 ( 1, 'Martedì', 'Rio de Janeiro', 'Tokyo', 'B737', 15, 23 );
