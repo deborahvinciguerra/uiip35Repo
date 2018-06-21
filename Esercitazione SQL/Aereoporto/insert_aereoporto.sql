34439abb736f4b4527c9dd25a0af19eb42a05d99
Antonio Pagliaro <antonio.pagliaro1988@gmail.com>
2018-06-21 11:33:45 +0200

    inserimento Pagliaro

---
+++ Esercitazione SQL/Aereoporto/insert_aereoporto.sql
@@ -0,0 +1,7 @@
+
+#inizio inserimento aeroporto Pagliaro
+INSERT INTO aeroporto (citta,nazione,npiste) VALUES
+					('Londra', 'Inghilterra',10);
+                    
+#fine inserimento aereoporto Pagliaro
+					
